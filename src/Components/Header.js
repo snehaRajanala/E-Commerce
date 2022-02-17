c35eb2bb078b7114902031e5d5c3e831852e19de
sharmasnehaa1828 <93026056+sharmasnehaa1828@users.noreply.github.com>
2022-02-17 09:05:20 +0530

    commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,14 +6,14 @@ function Header() {
   return (
     <div>
         <div className="nav-wrapper">  
-                <a style={{color:'black', padding:'10px'}}><img src={Logo} alt="" width="40" className='logo2' />yntra</a>
+                <a ><img src={Logo} alt="" width="50" className='logo2' />yntra</a>
                 <span>MAN</span>
                 <span>WOMAN</span>
                 <span>KIDS</span>
                 <span>HOME & LIVING</span>
                 <span>BEAUTY</span>
                 <input id="search" placeholder='Search' type="search" 
-                className='search-bar #e3f2fd blue lighten-5 modal-trigger'
+                className='search-bar '
                 />
                   
                 
